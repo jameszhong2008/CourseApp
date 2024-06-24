0c7ca6a7892b09fa8ff5b0a95b405fc16ba59b90
jameszhong2008 <zhongjian2008@gmail.com>
2024-06-24 22:30:35 +0800

    upgrade to react-native 0.74.2

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+### 使用 node 18
+
 ### 安装依赖
 
 1. yarn
@@ -22,3 +24,23 @@ try {
     // Do nothing. We can't handle this, and this is usually a system problem
 }
 ```
+
+### 升级 react-native
+
+1. 修改 package.json 中
+   "dependencies": {
+   "react-native": "0.74.2",
+   }
+2. npm/yarn install
+3. npx react-native upgrade 0.74.2
+
+### 升级后发现无法 pod install 无法安装
+
+Invalid `Podfile` file: 767: unexpected token at ''.
+
+删除 node_modules 后重新 install
+cd ios && pod install
+
+### Xcode Error PhaseScriptExecution failed with a nonzero exit code
+
+https://stackoverflow.com/questions/75975043/xcode-error-phasescriptexecution-failed-with-a-nonzero-exit-code
--- ios/CourseApp/AppDelegate.mm
+++ ios/CourseApp/AppDelegate.mm
@@ -10,56 +10,22 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-  
-  // [self copySupportFiles];
-  
+
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
-- (void)copySupportFiles
+- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
-  NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,
-                                                       NSUserDomainMask, YES);
-  if ([paths count] <= 0){
-    return;
-  }
-  NSLog(@"path_doctumnet %@", [paths objectAtIndex:0]);
-  NSString *_documentsDir = [paths objectAtIndex:0];
-  NSFileManager *fm = [NSFileManager defaultManager];
-  NSArray* localDocuments = [fm contentsOfDirectoryAtPath:_documentsDir error:nil];
-  NSArray * _demoFiles = [NSArray arrayWithObjects:@"", nil];
-  for(NSString *demoFile in _demoFiles){
-      NSURL *modelURL = [[NSBundle mainBundle] URLForResource:[demoFile stringByDeletingPathExtension] withExtension:[demoFile pathExtension]];
-      NSLog(@"fm %@", modelURL.path);
-      if([fm fileExistsAtPath:modelURL.path]){
-        NSURL *toURL = [NSURL fileURLWithPath:[_documentsDir stringByAppendingPathComponent:demoFile]];
-        [fm copyItemAtURL:modelURL toURL:toURL error:nil];
-          
-        NSLog(@"copy success: %@ ", demoFile);
-      }
-      else {
-        NSLog(@"copy failed: %@ ", demoFile);
-      }
-  }
+  return [self bundleURL];
 }
 
-- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
+- (NSURL *)bundleURL
 {
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
-}
-
-/// This method controls whether the `concurrentRoot`feature of React18 is turned on or off.
-///
-/// @see: https://reactjs.org/blog/2022/03/29/react-v18.html
-/// @note: This requires to be rendering on Fabric (i.e. on the New Architecture).
-/// @return: `true` if the `concurrentRoot` feature is enabled. Otherwise, it returns `false`.
-- (BOOL)concurrentRootEnabled
-{
-  return true;
 }
 
 @end
